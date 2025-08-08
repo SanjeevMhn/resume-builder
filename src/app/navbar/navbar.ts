3c26233b9c6e93d47cae2c2fd2d99f81e3f9be7c
SanjeevMhn <sanjeevmaharjan19@gmail.com>
2025-08-08 16:29:19 +0545

    [ADDED]: added template thumbnail and removed download button from navbar

--- src/app/navbar/navbar.ts
+++ src/app/navbar/navbar.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { LucideAngularModule, User } from 'lucide-angular';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [LucideAngularModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss'
 })
 export class Navbar {
-
+  userIcon = User
 }
